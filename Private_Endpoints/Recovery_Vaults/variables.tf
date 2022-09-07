3eb3bcc5338898491a4e12b2e03f42ee97884b91
NsamaChibulu <nsama.chibulu@avanade.com>
2022-09-07 17:30:04 +0100

    working progress

--- Private_Endpoints/Recovery_Vaults/variables.tf
+++ Private_Endpoints/Recovery_Vaults/variables.tf
@@ -1,4 +1,6 @@
 
+
+## Resource group 
 variable "RG_name" {
   description = "resoruce group with the keyvault"
   type        = string
@@ -7,21 +9,22 @@ variable "RG_name" {
 
 }
 
-
-
-variable "dns" {
+## DNS zones where the private endpoints will be added to 
+variable "dns_zones" {
   type = object({
-    zone_ids = lis(string)
+    zone_ids  = list(string)
     zone_name = string
   })
 }
 
+## Resource specification (to be pulled out of csv file)
 variable "resource_ID" {
   description = "the resource for which the private end point will be created for"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "subresouce_names" {
-  type = list(string)
+  type    = list(string)
   default = null
 }
+
