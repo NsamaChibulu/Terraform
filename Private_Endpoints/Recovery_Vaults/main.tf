ca9855529a2e3be5a72bf53b2779a33ee5b9e283
NsamaChibulu <nsama.chibulu@outlook.com>
2022-10-21 15:18:08 +0100

    New structure

--- Private_Endpoints/Recovery_Vaults/main.tf
+++
@@ -1,9 +0,0 @@
-## Private Link point create on existing resources
-
-## Subnet where Private endpoint will be created
-
-
-## Data lookups for the resources listed in CSV
-
-# Subnet verification
-
